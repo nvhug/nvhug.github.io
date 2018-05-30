8949688132e7fabd6f9a840680e39b51006dcec1
Nguyen Van Hung <hungnv@mti-tech.vn>
2018-05-30 15:24:46 +0700

    add highlightjs

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { withRouter } from "react-router-dom";
-import { connect } from 'react-redux';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
