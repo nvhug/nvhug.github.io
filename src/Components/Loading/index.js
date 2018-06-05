460da56242cbb4d04300f888321838c5fbcb5681
Nguyen Van Hung <hungnv@mti-tech.vn>
2018-06-05 16:30:51 +0700

    add Loading Component

---
+++ src/Components/Loading/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+
+class Loading extends Component {
+
+  render() {
+    return (
+    	<Row>
+        <Col xs={12} md={4} mdOffset={4}>
+          <Image src={require('../../Images/loading3.gif')} width="100%" />
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default Loading;
