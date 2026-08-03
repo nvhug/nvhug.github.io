30bb037d079223f4c909f0e3858ad6f6d7f3c9c2
NoteSpace <vhcntt@gmail.com>
2026-08-04 06:37:23 +0700

    add features

--- src/types/index.ts
+++ src/types/index.ts
@@ -88,7 +88,7 @@ export interface BuyPick {
   brands: string[]
   note?: string
   order_index: number
-  is_purchased: boolean
+  purchase_count: number
   created_at: string
   updated_at: string
 }
